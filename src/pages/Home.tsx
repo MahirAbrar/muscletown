c482621e5408ab618efc702ce91116cc77b4d434
Mahir Abrar <hamid.mahir2597@Yahoo.com>
2024-11-07 15:53:44 +1100

    add logos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,11 @@
-import React, { useRef } from "react";
-import {
-  motion,
-  useScroll,
-  useSpring,
-  useTransform,
-  MotionValue,
-} from "framer-motion";
-
-import gymFitnessSvg from "../assets/gym-fitness-svgrepo-com.svg";
-import gymStationSvg from "../assets/gym-station-svgrepo-com.svg";
-import gymSvg from "../assets/gym-svgrepo-com.svg";
-
-function useParallax(value: MotionValue<number>, distance: number) {
-  return useTransform(value, [0, 1], [-distance, distance]);
-}
+import React from "react";
+import { useInView, motion } from "framer-motion";
+import { useRef } from "react";
 
 function Section({
   id,
   bgClass,
-  height = "h-screen", // Add height prop with default value
+  height = "",
   children,
 }: {
   id?: string;
@@ -26,34 +13,10 @@ function Section({
   height?: string;
   children: React.ReactNode;
 }) {
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start start", "end start"], // This means the animation starts when the section's start hits the viewport's start
-  });
-
-  const y = useParallax(
-    useSpring(scrollYProgress, {
-      stiffness: 50,
-      damping: 30,
-      mass: 0.5,
-    }),
-    50,
-  );
-
   return (
-    <section className={`${bgClass} relative ${height}`}>
-      <div ref={ref} className="h-screen snap-center">
-        {children}
-      </div>
-      {id && (
-        <motion.div
-          style={{ y }}
-          className="absolute right-20 text-4xl font-bold text-brand-primary/20 dark:text-brand-secondary/20"
-        >
-          #{id}
-        </motion.div>
-      )}
+    <section className={`${bgClass} ${height}`}>
+      {/* add snap property or something */}
+      {children}
     </section>
   );
 }
@@ -85,117 +48,132 @@ const FitnessIllustration = () => (
 );
 
 const Home = () => {
-  const { scrollYProgress } = useScroll({
-    offset: ["20vh", "80vh"], // This means the animation starts after scrolling 20% of viewport height and ends at 80%
-  });
-
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 50,
-    damping: 30,
-    mass: 0.5,
-  });
+  const sponsorRef = useRef(null);
+  const isInView = useInView(sponsorRef, { once: true });
 
   return (
-    <>
-      <motion.div
-        className="fixed left-0 right-0 top-0 z-50 h-2 origin-[0%] bg-brand-primary dark:bg-brand-secondary"
-        style={{ scaleX }}
-      />
-
-      <div className="h-screen snap-y snap-mandatory overflow-y-scroll bg-background-primary dark:bg-background-secondary">
-        <Section id="home" bgClass="bg-none" height="h-5/6">
-          <div className="container mx-auto h-full px-6">
-            <div className="flex h-full flex-wrap items-center justify-between gap-8 lg:flex-nowrap">
-              {/* Left side */}
-              <div className="flex w-full flex-col space-y-10 pl-0 pt-12 lg:w-1/2 lg:pl-4 lg:pt-0">
-                <div className="relative flex flex-col items-start">
-                  {" "}
-                  <h2 className="absolute -top-8 text-left text-7xl font-bold text-brand-primary opacity-10 md:-top-20 md:left-4 md:text-9xl dark:text-brand-secondary">
-                    MuscleTown
-                  </h2>
-                  <h1 className="text-left text-6xl font-bold text-brand-primary dark:text-brand-secondary">
-                    MuscleTown
-                  </h1>
-                  <h2 className="mt-2 text-left text-3xl font-semibold text-text-primary dark:text-text-secondary">
-                    Evolutionary fitness
-                  </h2>
-                </div>
-                <p className="max-w-xl text-left text-lg text-text-primary dark:text-text-secondary">
-                  Unrivaled Gym, Unparalleled Training Fitness Classes. World
-                  Class Equipments to get the Body Shapes that you Dream of. Get
-                  your Dream Body Now.
-                </p>
-                <div className="flex space-x-4 pt-4">
-                  <button className="rounded-lg bg-brand-primary px-8 py-3 font-semibold text-text-secondary transition-colors duration-300 hover:bg-brand-primaryLight">
-                    Join Now
-                  </button>
-                  <button className="rounded-lg border-2 border-brand-primary px-8 py-3 font-semibold text-brand-primary transition-colors duration-300 hover:bg-brand-primary hover:text-text-secondary">
-                    Learn More
-                  </button>
-                </div>
+    <div className="h-screen overflow-y-scroll bg-background-primary dark:bg-background-secondary">
+      <Section id="home" bgClass="bg-none" height="h-[800px] lg:h-[600px]">
+        <div className="h- container mx-auto items-center justify-center px-6">
+          <div className="flex flex-wrap items-center justify-between gap-2 lg:flex-nowrap">
+            {/* Left side */}
+            <div className="flex w-full flex-col space-y-10 pl-0 pt-12 lg:w-1/2 lg:pl-4 lg:pt-0">
+              <div className="relative flex flex-col items-start">
+                {" "}
+                <h2 className="absolute -top-8 text-left text-7xl font-bold text-brand-primary opacity-10 md:-top-20 md:left-4 md:text-9xl dark:text-brand-secondary">
+                  MuscleTown
+                </h2>
+                <h1 className="text-left text-6xl font-bold text-brand-primary dark:text-brand-secondary">
+                  MuscleTown
+                </h1>
+                <h2 className="mt-2 text-left text-3xl font-semibold text-text-primary dark:text-text-secondary">
+                  Evolutionary fitness
+                </h2>
               </div>
-
-              {/* Right side */}
-              <div className="flex h-[700px] w-full items-center justify-center lg:w-1/2">
-                <img
-                  src="/heroImage.png"
-                  alt="Fitness training"
-                  className="relative z-10 h-full w-full object-contain"
-                />
+              <p className="max-w-xl text-left text-lg text-text-primary dark:text-text-secondary">
+                Unrivaled Gym, Unparalleled Training Fitness Classes. World
+                Class Equipments to get the Body Shapes that you Dream of. Get
+                your Dream Body Now.
+              </p>
+              <div className="flex space-x-4 pt-4">
+                <button className="rounded-lg bg-brand-primary px-8 py-3 font-semibold text-text-secondary transition-colors duration-300 hover:bg-brand-primaryLight">
+                  Join Now
+                </button>
+                <button className="rounded-lg border-2 border-brand-primary px-8 py-3 font-semibold text-brand-primary transition-colors duration-300 hover:bg-brand-primary hover:text-text-secondary">
+                  Learn More
+                </button>
               </div>
             </div>
+
+            {/* Right side */}
+            <div className="flex h-[380px] w-full justify-center lg:h-[600px]">
+              <img
+                src="/heroImage.png"
+                alt="Fitness training"
+                className="relative z-10 h-full w-screen"
+              />
+            </div>
           </div>
-        </Section>
+        </div>
+      </Section>
 
-        <Section bgClass="bg-background-secondary dark:bg-background-primary">
-          <h1 className="text-center text-4xl font-bold text-brand-primary dark:text-brand-secondary">
-            Some brand logos
-          </h1>
-        </Section>
-
-        <Section id="about" bgClass="bg-none">
-          <h1 className="pt-20 text-center text-4xl font-bold text-brand-primary dark:text-brand-secondary">
-            More than just a gym, a community
-          </h1>
-        </Section>
-
-        <Section bgClass="bg-background-secondary dark:bg-background-primary">
-          <h1 className="text-center text-4xl font-bold text-brand-primary dark:text-brand-secondary">
-            Some brand logos
-          </h1>
-        </Section>
-
-        <Section id="about" bgClass="bg-none">
-          <h1 className="pt-20 text-center text-4xl font-bold text-brand-primary dark:text-brand-secondary">
-            More than just a gym, a community
-          </h1>
-        </Section>
-
-        <Section
-          id="classes"
-          bgClass="bg-background-secondary dark:bg-background-primary"
+      <Section bgClass="bg-background-secondary dark:bg-background-primary p-4 md:p-8">
+        <div
+          className="flex flex-col items-center justify-around space-y-8 md:flex-row md:space-y-0"
+          ref={sponsorRef}
         >
-          <h1 className="pt-20 text-center text-4xl font-bold text-brand-primary dark:text-brand-secondary">
-            Our classes
-          </h1>
-        </Section>
+          <motion.div
+            initial={{ opacity: 0, x: -100 }}
+            animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: -100 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+            className="flex w-fit flex-col items-center justify-center"
+          >
+            <img
+              src="/favicon.ico"
+              alt="Portfolio Icon"
+              className="h-12 w-12 md:h-16 md:w-16 lg:h-20 lg:w-20"
+            />
+            <h2 className="mt-2 text-sm md:text-base lg:text-lg">
+              Mahirs Portfolio
+            </h2>
+          </motion.div>
+          <motion.img
+            initial={{ opacity: 0, x: -100 }}
+            animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: -100 }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+            src="/Forbessvg.svg"
+            alt="Forbes"
+            className="h-12 md:h-16 lg:h-20"
+          />
+          <motion.img
+            initial={{ opacity: 0, x: -100 }}
+            animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: -100 }}
+            transition={{ duration: 0.5, delay: 0.6 }}
+            src="/fortune-seeklogo.svg"
+            alt="Fortune"
+            className="h-8 md:h-9 lg:h-10"
+          />
+          <motion.img
+            initial={{ opacity: 0, x: -100 }}
+            animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: -100 }}
+            transition={{ duration: 0.5, delay: 0.8 }}
+            src="/SponsorRedBull.png"
+            alt="RedBull"
+            className="h-12 object-contain md:h-16 lg:h-20"
+          />
+        </div>
+      </Section>
 
-        <Section id="prices" bgClass="bg-none">
-          <h1 className="pt-20 text-center text-4xl font-bold text-brand-primary dark:text-brand-secondary">
-            Prices
-          </h1>
-        </Section>
+      <Section id="about" bgClass="bg-none">
+        <h1 className="pt-20 text-center text-4xl font-bold text-brand-primary dark:text-brand-secondary">
+          More than just a gym, a community
+        </h1>
+      </Section>
 
-        <Section
-          id="join"
-          bgClass="bg-background-secondary dark:bg-background-primary"
-        >
-          <h1 className="pt-20 text-center text-4xl font-bold text-brand-primary dark:text-brand-secondary">
-            Join now to be healthy and conquer the world
-          </h1>
-        </Section>
-      </div>
-    </>
+      <Section
+        id="classes"
+        bgClass="bg-background-secondary dark:bg-background-primary"
+      >
+        <h1 className="pt-20 text-center text-4xl font-bold text-brand-primary dark:text-brand-secondary">
+          Our classes
+        </h1>
+      </Section>
+
+      <Section id="prices" bgClass="bg-none">
+        <h1 className="pt-20 text-center text-4xl font-bold text-brand-primary dark:text-brand-secondary">
+          Prices
+        </h1>
+      </Section>
+
+      <Section
+        id="join"
+        bgClass="bg-background-secondary dark:bg-background-primary"
+      >
+        <h1 className="pt-20 text-center text-4xl font-bold text-brand-primary dark:text-brand-secondary">
+          Join now to be healthy and conquer the world
+        </h1>
+      </Section>
+    </div>
   );
 };
 
